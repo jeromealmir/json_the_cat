0385f7ea205a2acdd44bc3644d3781f73fa8fe73
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-23 02:13:10 -0800

    remove commented out code

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,13 +5,7 @@ const fetchBreedDescription = (breedName, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
     if (error) callback(error, null);
-
-    // if (response && response.statusCode === 404) {
-    //   callback('Request failed :(');
-    //   callback('Status code: ', response && response.statusCode);
-    // }
-
-    //convert JSON to object
+    
     const parsedData = JSON.parse(body)[0];
 
     if (parsedData === undefined) {
