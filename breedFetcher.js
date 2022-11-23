faafe0140b2d001b21dcff6cd7f0889340481f86
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-23 02:06:16 -0800

    finish refactor and cleanup code

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,23 +4,23 @@ const fetchBreedDescription = (breedName, callback) => {
 
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
-  if (error) callback(error, null);
+    if (error) callback(error, null);
 
-  // if (response && response.statusCode === 404) {
-  //   callback('Request failed :(');
-  //   callback('Status code: ', response && response.statusCode);
-  // }
+    // if (response && response.statusCode === 404) {
+    //   callback('Request failed :(');
+    //   callback('Status code: ', response && response.statusCode);
+    // }
 
-  //convert JSON to object
-  const parsedData = JSON.parse(body)[0];
+    //convert JSON to object
+    const parsedData = JSON.parse(body)[0];
 
-  if (parsedData === undefined) {
-    callback(error, null);
-  } else {
-   callback(null, parsedData['description']);
-  }
+    if (parsedData === undefined) {
+      callback(error, null);
+    } else {
+      callback(null, parsedData['description']);
+    }
 
-})
+  });
 };
 
 module.exports = fetchBreedDescription;
