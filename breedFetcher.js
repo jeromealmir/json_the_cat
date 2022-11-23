2910c2f056f7cc900f0113817c014b1fee309c9d
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-23 01:51:23 -0800

    rename function to fetchBreedDescription and refactor to pass data to callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,31 +1,27 @@
 const request = require('request');
-const cli = process.argv[2].toString();
 
-const breedFetcher = (breed) => {
+const fetchBreedDescription = (breedName, callback) => {
 
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
-    if (error) {
-      console.error('Invalid URL!', error);
-      return;
-    }
+  if (error) callback(error, null);
 
-    if (response && response.statusCode === 404) {
-      console.log('Request failed :(');
-      console.log('Status code: ', response && response.statusCode);
-      return;
-    }
-    
-    //convert JSON to object
-    const parsedData = JSON.parse(body)[0];
+  // if (response && response.statusCode === 404) {
+  //   callback('Request failed :(');
+  //   callback('Status code: ', response && response.statusCode);
+  // }
 
-    if (parsedData === undefined) {
-      console.log('Breed not found :(');
-    } else {
-      console.log(parsedData['description']);
-    }
-  
-  });
+  //convert JSON to object
+  const parsedData = JSON.parse(body)[0];
+
+  if (parsedData === undefined) {
+    callback(error, null);
+  } else {
+   callback(null, parsedData['description']);
+  }
+
+})
 };
 
-breedFetcher(cli);
+module.exports = fetchBreedDescription;
+
