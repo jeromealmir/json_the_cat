6d28be2bc8f2564d9d4e90c4132319e9cab3e020
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-23 01:27:19 -0800

    implement function that performs an cat api search and return breed description

---
+++ breedFetcher.js
@@ -0,0 +1,30 @@
+const request = require('request');
+const cli = process.argv[2].toString();
+
+const breedFetcher = (breed) => {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+
+    if (error) {
+      console.error('Invalid URL!', error);
+      return;
+    }
+
+    if (response && response.statusCode === 404) {
+      console.log('Request failed :(');
+      console.log('Status code: ', response && response.statusCode);
+      return;
+    }
+    
+    const parsedData = JSON.parse(body)[0];
+
+    if (parsedData === undefined) {
+      console.log('Breed not found :(');
+    } else {
+      console.log(parsedData['description']);
+    }
+  
+  });
+};
+
+breedFetcher(cli);
